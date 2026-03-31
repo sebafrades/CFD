838fcc2da9fe81faf3cb46a7d00368ccf4572265
sebafrades <sebastianfrades@gmail.com>
2026-03-31 18:45:19 -0300

    Changes

--- Ejercicio_1.cpp
+++ Ejercicio_1.cpp
@@ -6,12 +6,12 @@ using namespace std;
 #define M 8
 
 int main(){
-    double x[M][N];
-    double y[M][N];
+    double x[M+1][N+1];
+    double y[M+1][N+1];
 
     // valores de x e y iniciales
-    for(int i=0; i<M; i++){
-        for(int j=0; j<N; j++){
+    for(int i=0; i<M+1; i++){
+        for(int j=0; j<N+1; j++){
             x[i][j] = 0.0;
             y[i][j] = 0.0;
         }
@@ -19,45 +19,51 @@ int main(){
 
     // contorno de x
     // fila superior e inferior
-    for(int i=0; i<N; i++){
+    for(int i=0; i<N+1; i++){
         x[0][i] = 0.0 + double(i);
-        x[M-1][i] = 0.0 + double(i);
+        x[M][i] = 0.0 + double(i);
     }
 
     // columna izquierda y derecha
-    for(int i=0; i<M; i++){
+    for(int i=0; i<M+1; i++){
         x[i][0] = 0.0;
-        x[i][N-1] = double(N);
+        x[i][N] = double(N);
     }
 
     // contorno de y
     // fila superior e inferior
-    for(int i=0; i<N; i++){
+    for(int i=0; i<N+1; i++){
         y[0][i] = double(M);
 
         if (i < N/4) {
-            y[M-1][i] = 0.0;
+            y[M][i] = 0.0;
         }
         else if (i >= N/4 && i < 3*N/8){
-            y[M-1][i] = double(i - N/4);
+            y[M][i] = double(i - N/4);
         }
         else if (i >= 3*N/8 && i < 5*N/8){
-            y[M-1][i] = double(2*M/8);
+            y[M][i] = double(2*M/8);
         }
         else if (i >= 5*N/8 && i < 3*N/4){
-            y[M-1][i] = double(2*M/8 - (i - 5*N/8));
+            y[M][i] = double(2*M/8 - (i - 5*N/8));
         }
         else if (i >= 3*N/4 && i < N){
-            y[M-1][i] = 0.0;
+            y[M][i] = 0.0;
         }
     }
 
-
-
-
-    for(int i=0; i<N; i++){
-        cout << y[M-1][i] << " ";
-    }        
+    // columna izquierda y derecha
+    for(int i=0; i<M+1; i++){
+        y[i][0] = double(M-i);
+        y[i][N] = double(M-i);
+    } 
+    
+    for(int i=0; i<M+1; i++){
+        for(int j=0; j<N+1; j++){
+            cout << x[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
 
